2be98bb2a989a6775d39cfce25a87daeb766dd11
Safwan Ehfaz Saad <208826388+safwanehfaz@users.noreply.github.com>
2026-01-28 19:02:34 +0600

    feat: implement basic http server
    - Serve "Hello, World!" on root path
    - Listen on port 80

--- main.go
+++ main.go
@@ -1,10 +1,25 @@
+// Step 1
+// package main
+
+// import (
+// 	"fmt"
+// )
+
+// // main is the entry point of the program.
+// func main() {
+// 	fmt.Println("Hello, World!")
+// }
+
+// Step 2
 package main
 
-import (
-	"fmt"
-)
+import "net/http"
+import "fmt"
 
-// main is the entry point of the program.
 func main() {
-	fmt.Println("Hello, World!")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "<h1>Hello, World!</h1>")
+    })
+
+    http.ListenAndServe(":80", nil)
 }
