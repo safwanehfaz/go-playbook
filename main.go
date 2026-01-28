2568a86ede4eb666299770a0f19778df4818b9c0
Safwan Ehfaz Saad <208826388+safwanehfaz@users.noreply.github.com>
2026-01-28 19:05:25 +0600

    feat(web): Add personalized greeting with form
    - Implement web form for name input
    - Display personalized greeting if name given
    - Show input form if no name provided

--- main.go
+++ main.go
@@ -11,14 +11,44 @@
 // }
 
 // Step 2
+// package main
+
+// import "net/http"
+// import "fmt"
+
+// func main() {
+//     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+//         fmt.Fprintf(w, "<h1>Hello, World!</h1>")
+//     })
+
+//     http.ListenAndServe(":80", nil)
+// }
+
+// Step 3
 package main
 
-import "net/http"
 import "fmt"
+import "net/http"
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "<h1>Hello, World!</h1>")
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        if r.FormValue("q") != "" {
+            fmt.Fprintf(w, `
+                <body>
+                    <h1>Hello, %s</h1>
+                </body>
+            `, r.FormValue("q"))
+            return
+        }
+        fmt.Fprintf(w, `
+            <body>
+                <form action="/" method="GET">
+                    <label>Enter your name</label>
+                    <input name="q">
+                    <button type="submit">Submit</button>
+                </form>
+            </body>
+        `)
     })
 
     http.ListenAndServe(":80", nil)
